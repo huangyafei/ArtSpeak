6cb71cada212151ba029a3d25878a294aca0357c
huangyafei <yafei.huangfu@gmail.com>
2023-08-07 16:13:00 +0800

    add umami analytics

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -10,6 +10,7 @@ class MyDocument extends Document {
             name="description"
             content="将你的句子用当代艺术的语言输出"
           />
+          <script async src="https://analytics.huangyafei.com/script.js" data-website-id="b7663263-bc40-4bd3-9d1a-b07590fd32ed"></script>
         </Head>
         <body className="bg-yellow-300">
           <Main />
